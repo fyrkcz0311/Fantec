615500242039956e3bd1b79edfd1feaa543c4f8b
Fernando <fyrkcz0311@gmail.com>
2025-06-08 22:58:12 -0500

    Arreglos

--- src/main/java/com/example/demo/model/Contacto.java
+++ src/main/java/com/example/demo/model/Contacto.java
@@ -16,20 +16,20 @@ public class Contacto {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "El correo es obligatorio.")
+    @NotBlank(message = "El correo es obligatorio.")
     @Email(message = "Por favor ingresa un correo válido.")
     private String email;
 
-    @NotNull(message = "El nombre es obligatorio.")
+    @NotBlank(message = "El nombre es obligatorio.")
     @Size(min = 2, max = 50, message = "El nombre debe tener entre 2 y 50 caracteres.")
     @Pattern(regexp = "^[A-Za-z\\s]+$", message = "El nombre solo puede contener letras y espacios.")
     private String name;
 
-    @NotNull(message = "El teléfono es obligatorio.")
+    @NotBlank(message = "El teléfono es obligatorio.")
     @Pattern(regexp = "^[0-9]+$", message = "El teléfono solo puede contener números.")
     private String phone;
 
-    @NotNull(message = "El mensaje es obligatorio.")
+    @NotBlank(message = "El mensaje es obligatorio.")
     @Size(min = 5, message = "El mensaje debe tener al menos 5 caracteres.")
     private String message;
 }
